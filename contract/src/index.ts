82a8b5a7b1a8597c8091114623bc6e3a5f05cbc9
Gyan <gyanprakasht987@gmail.com>
2026-09-15 20:47:17 +0530

    Refactor CrediFi contract entrypoint
    Refactor contract imports and update compiled contract creation.

--- contract/src/index.ts
+++ contract/src/index.ts
@@ -1,29 +1,22 @@
 
-// CrediFi contract compilation entrypoint.
-// SPDX-License-Identifier: Apache-2.0
-//
-// Licensed under the Apache License, Version 2.0.
-// You may obtain a copy of the License at
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is provided on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 import { CompiledContract } from "@midnight-ntwrk/midnight-js-protocol/compact-js";
+
+import * as ContractModule from "./managed/credifi/contract/index.js";
+import * as WitnessModule from "./witnesses";
+
+export type CrediFiContractInstance =
+  ContractModule.Contract<WitnessModule.CrediFiPrivateState>;
+
+export const CrediFiCompiledContract = CompiledContract.make<
+  CrediFiContractInstance
+>(
+  "CrediFi",
+  ContractModule.Contract<WitnessModule.CrediFiPrivateState>,
+).pipe(
+  CompiledContract.withWitnesses(WitnessModule.witnesses),
+  CompiledContract.withCompiledFileAssets("./managed/credifi"),
+);
 
 export * from "./managed/credifi/contract/index.js";
 export * from "./witnesses";
-
-import * as CrediFiContract from "./managed/credifi/contract/index.js";
-import * as CrediFiWitnesses from "./witnesses";
-
-export const CompiledCrediFiContract = CompiledContract.make<
-  CrediFiContract.Contract<CrediFiWitnesses.CrediFiPrivateState>
->("CrediFi", CrediFiContract.Contract<CrediFiWitnesses.CrediFiPrivateState>).pipe(
-  CompiledContract.withWitnesses(CrediFiWitnesses.witnesses),
-  CompiledContract.withCompiledFileAssets("./managed/credifi"),
-);
 
